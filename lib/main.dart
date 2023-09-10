fcb5ac8d91ec7ff11eb7580ee1e159a23321bafb
sangwoo <tkddn3367@gmail.com>
2023-09-10 11:23:36 +0900

    rename the App

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const IfTimerApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class IfTimerApp extends StatelessWidget {
+  const IfTimerApp({super.key});
 
   // This widget is the root of your application.
   @override
